924476ff3871909d2d84168038449af32f4fe8b5
TyshchenkoYulia <veshkina.yulia021088@gmail.com>
2023-12-17 13:18:46 +0200

    update

---
+++ js/task-1.js
@@ -0,0 +1,9 @@
+function calculateTotal(number) {
+    let total = 0;
+    for (let sum = 0, sum <= number, sum ++) {
+      total += sum;
+      return total;
+    }
+         
+  }
+  console.log (calculateTotal(1))
